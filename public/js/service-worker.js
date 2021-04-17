f7b23b9f5ecc78fd63b5191e2581e5780ac26264
bluesatyr <shawnevans.music@gmail.com>
2021-04-17 16:11:25 -0400

    alter service-worker.js

--- public/js/service-worker.js
+++ public/js/service-worker.js
@@ -1,9 +1,19 @@
 const FILES_TO_CACHE = [
-    "/index.html",
-    "/css/style.css",
-    "/js/index.js",
-    "/js/idb.js",
-  ];
+  "/",  
+  "/index.html",
+  "/css/style.css",
+  "/js/index.js",
+  "/js/idb.js",
+  "/manifest.json",
+  "/icons/icon-72x72.png",
+  "/icons/icon-96x96.png",
+  "/icons/icon-128x128.png",
+  "/icons/icon-144x144.png",
+  "/icons/icon-152x152.png",
+  "/icons/icon-192x192.png",
+  "/icons/icon-384x384.png",
+  "/icons/icon-512x512.png"
+];
 
   const CACHE_NAME = 'budget-tracker-cache-v1';
   const DATA_CACHE_NAME = 'data-cache-v1';
