759e370696ca4a6bd143cd02210769c435f6d1de
bluesatyr <shawnevans.music@gmail.com>
2021-04-17 17:34:14 -0400

    fix service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,23 +1,23 @@
 const FILES_TO_CACHE = [
-  "./",  
-  "./index.html",
-  "./css/style.css",
-  "./js/index.js",
-  "./js/idb.js",
-  "./manifest.json",
-  "./icons/icon-72x72.png",
-  "./icons/icon-96x96.png",
-  "./icons/icon-128x128.png",
-  "./icons/icon-144x144.png",
-  "./icons/icon-152x152.png",
-  "./icons/icon-192x192.png",
-  "./icons/icon-384x384.png",
-  "./icons/icon-512x512.png"
+    "./",
+    "./index.html",
+    "./css/styles.css",
+    "./manifest.json",
+    "./js/idb.js",
+    "./js/index.js",
+    "./icons/icon-72x72.png",
+    "./icons/icon-96x96.png",
+    "./icons/icon-128x128.png",
+    "./icons/icon-144x144.png",
+    "./icons/icon-152x152.png",
+    "./icons/icon-192x192.png",
+    "./icons/icon-384x384.png",
+    "./icons/icon-512x512.png"
 ];
 
-  const CACHE_NAME = 'budget-tracker-cache-v1';
-  const DATA_CACHE_NAME = 'data-cache-v1';
-  
+const APP_PREFIX = 'BudgetTracker-';     
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
 
 self.addEventListener('install', function (e) {
     e.waitUntil(
@@ -25,28 +25,28 @@ self.addEventListener('install', function (e) {
         console.log('installing cache : ' + CACHE_NAME)
         return cache.addAll(FILES_TO_CACHE)
       })
+    )
+});
+
+self.addEventListener('activate', function(e) {
+    e.waitUntil(
+      caches.keys().then(function(keyList) {
+        let cacheKeeplist = keyList.filter(function(key) {
+          return key.indexOf(APP_PREFIX);
+        });
+        cacheKeeplist.push(CACHE_NAME);
+  
+        return Promise.all(
+          keyList.map(function(key, i) {
+            if (cacheKeeplist.indexOf(key) === -1) {
+              console.log('deleting cache : ' + keyList[i]);
+              return caches.delete(keyList[i]);
+            }
+          })
+        );
+      })
     );
-    self.skipWaiting();
 });
-
-// Activate the service worker and remove old data from the cache
-self.addEventListener('activate', function(evt) {
-  evt.waitUntil(
-    caches.keys().then(keyList => {
-      return Promise.all(
-        keyList.map(key => {
-          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-            console.log('Removing old cache data', key);
-            return caches.delete(key);
-          }
-        })
-      );
-    })
-  );
-
-  self.clients.claim();
-});
-
 
 self.addEventListener('fetch', function (e) {
     console.log('fetch request : ' + e.request.url)
